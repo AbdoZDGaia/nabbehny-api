5425f73e7dcd4614d8257a578113d82390ed494e
Abdelrahman Selim <abdelrahmanseliem5@gmail.com>
2023-10-18 19:26:20 +0300

    added children modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckController } from './health-check/health-check.controller';
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [],
+  imports: [SharedModule, UserModule],
   controllers: [HealthCheckController],
   providers: [],
 })
